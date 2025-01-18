fe5c797fa725373b90d3e0e931ae8d55ac872efd
CMS <42698646+charliems@users.noreply.github.com>
2025-01-18 13:57:20 +0000

    fix: change bitwarden installation methond

--- files/scripts/bw.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Downloading latest Bitwarden release..."
-curl -Ls \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/bitwarden/clients/releases/latest | jq -r '.assets[] | select(.name | endswith(".rpm")) | .browser_download_url' | rpm-ostree install -y $(</dev/stdin)
-
